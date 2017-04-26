80697f8b2a70721f4011cf23029f3a4e249deac1
Kelly Clemons <kelly.clemons@mac.com>
2017-04-26 14:12:08 -0500

    created routes

---
+++ app/routes/application.vue
@@ -0,0 +1,24 @@
+<template lang="html">
+  <div class="">
+
+  </div>
+</template>
+
+<script>
+import Index from '.index.vue';
+export default {
+  components: {
+    Index,
+  },
+
+  name: 'Application',
+  data() {
+    return {
+    };
+  },
+
+  methods: {
+
+  },
+};
+</script>
---
+++ app/routes/index.vue
@@ -0,0 +1,19 @@
+<template lang="html">
+  <div class="">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+  data() {
+    return {
+    };
+  },
+
+  methods: {
+
+  },
+};
+</script>
